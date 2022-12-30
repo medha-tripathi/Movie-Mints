eba93df95e60387bad0f1d4759b43193ed040376
MEDHA TRIPATHI <102402026+medha-tripathi@users.noreply.github.com>
2022-12-30 19:45:49 +0530

    fc

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from 'react-router-dom';
+import { AppProvider } from './components/context';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+  <React.StrictMode>
+  <AppProvider>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+  </AppProvider>
+  </React.StrictMode>
+ 
+);
+reportWebVitals();
